b1db1c9ba720befeb937dd2584c31cab41241514
elvis <poggi.elvis@gmail.com>
2017-11-03 17:54:15 +0100

    connects app to store and renders Debug component with store props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
+import Debug from './components/Debug'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render () {
+    const tradeStyle = {
+      textAlign: 'center',
+      width: '50px',
+      display: 'inline-block'
+    }
+    const tickerStyle = {
+      textAlign: 'center',
+      display: 'inline-block',
+      float: 'right'
+    }
+    const bookStyle = {
+      textAlign: 'center',
+      display: 'inline-block',
+      float: 'left'
+    }
+    const { trades, tickers, bids, asks } = this.props
     return (
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={tradeStyle}>Trades: <Debug data={trades} /></div>
+        <div style={tickerStyle}>Ticker: <Debug data={tickers} /></div>
+        <div style={bookStyle}>Book:
+          <div style={{float: 'left', margin: '10px', display: 'inline-block'}}>
+            Bids:<Debug data={bids} />
+          </div>
+          <div style={{margin: '10px', display: 'inline-block'}}>
+            Asks:<Debug data={asks} />
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps (state) {
+  return {
+    trades: state.trades,
+    tickers: state.tickers,
+    bids: state.books.bids,
+    asks: state.books.asks
+  }
+}
+
+export default connect(mapStateToProps)(App)
