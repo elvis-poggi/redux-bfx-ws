2d4059fb25cc121d03cf789060ca722027355801
elvis <poggi.elvis@gmail.com>
2017-11-06 15:39:45 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -6,34 +6,19 @@ import { connect } from 'react-redux'
 
 class App extends Component {
   render () {
-    const tradeStyle = {
-      textAlign: 'center',
-      width: '50px',
-      display: 'inline-block'
-    }
-    const tickerStyle = {
-      textAlign: 'center',
-      display: 'inline-block',
-      float: 'right'
-    }
-    const bookStyle = {
-      textAlign: 'center',
-      display: 'inline-block',
-      float: 'left'
-    }
     const { trades, tickers, bids, asks } = this.props
     return (
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <div style={tradeStyle}>Trades: <Debug data={trades} /></div>
-        <div style={tickerStyle}>Ticker: <Debug data={tickers} /></div>
-        <div style={bookStyle}>Book:
-          <div style={{float: 'left', margin: '10px', display: 'inline-block'}}>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <div >Trades: <Debug data={trades} /></div>
+          <div >Ticker: <Debug data={tickers} /> </div>
+          <div >
             Bids:<Debug data={bids} />
           </div>
-          <div style={{margin: '10px', display: 'inline-block'}}>
+          <div>
             Asks:<Debug data={asks} />
           </div>
         </div>
