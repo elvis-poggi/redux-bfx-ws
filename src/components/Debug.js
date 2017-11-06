14a288aa10dcb45ecfaaf1e659e83acd7935c749
elvis <poggi.elvis@gmail.com>
2017-11-06 15:39:24 +0100

    minor fix

--- src/components/Debug.js
+++ src/components/Debug.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Debug = (props = {}) => {
   const { data } = props
   return (
-    <pre>{JSON.stringify(data, 0, 2)}</pre>
+    <pre style={{textAlign: 'left'}}>{JSON.stringify(data, 0, 2)}</pre>
   )
 }
 
