76ab37a475c3abb1430950af1b4049c6a85a838e
elvis <poggi.elvis@gmail.com>
2017-11-07 17:30:05 +0100

    define max number of stored Trades

--- src/redux/reducers/trades.reducer.js
+++ src/redux/reducers/trades.reducer.js
@@ -1,18 +1,27 @@
+import { reverse, sortBy, slice } from 'lodash'
+
+const MAX_LENGTH = 20
+
+function filterTrades (data) {
+  const sortedData = reverse(sortBy(data, 'mts'))
+  return slice(sortedData, 0, MAX_LENGTH)
+}
+
 const trades = (state = {}, action = {}) => {
   const { type, payload = {} } = action
   switch (type) {
     case 'TRADE_UPDATE': {
-      return {
+      return filterTrades({
         ...state,
         [payload.tradeId]: payload
-      }
+      })
     }
     case 'TRADE_SNAPSHOT': {
       let result = {}
       for (const row of payload) {
         result[ row.tradeId ] = row
       }
-      return result
+      return filterTrades(result)
     }
 
     default:
