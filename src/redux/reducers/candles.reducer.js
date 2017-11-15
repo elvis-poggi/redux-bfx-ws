762fc934c10ad5f9ba4274de88a89cf5e60fb137
elvis <poggi.elvis@gmail.com>
2017-11-15 14:04:26 +0100

    manages candle update

--- src/redux/reducers/candles.reducer.js
+++ src/redux/reducers/candles.reducer.js
@@ -1,25 +1,38 @@
+import {findIndex} from 'lodash'
+
+function isUpdate (data = [], msg = {}) {
+  return findIndex(data, ['mts', msg.mts])
+}
+
 const candles = (state = {}, action = {}) => {
   const { type, payload = {} } = action
   switch (type) {
     case 'CANDLE_UPDATE': {
-      const { data = {} } = payload
-      return {
-        ...state,
-        [payload.symbol]: {
-          ...state[payload.symbol],
-          [data.mts]: data.close
+      const { data = {}, symbol } = payload
+      const index = isUpdate(state[symbol], data)
+      if (index !== -1) {
+        console.log('candle update is updating')
+        return {
+          [symbol]: [
+            ...state[symbol].slice(0, index),
+            data,
+            ...state[symbol].slice(index + 1)
+          ]
+        }
+      } else {
+        console.log('candle update is inserting')
+        return {
+          [symbol]: [
+            ...state[symbol],
+            data
+          ]
         }
       }
     }
     case 'CANDLE_SNAPSHOT': {
-      let result = {}
-      const { data = {} } = payload
-      for (const row of data) {
-        result[ row.mts ] = row.close
-      }
+      const { data = {}, symbol } = payload
       return {
-        ...state,
-        [payload.symbol]: result
+        [symbol]: data
       }
     }
     default:
